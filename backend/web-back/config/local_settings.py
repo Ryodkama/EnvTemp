af264851e9b6afe16db01b2c15baab57c1a9a3c1
Ryosuke <ryosuke.kamada.1998@gmail.com>
2022-12-08 21:05:30 +0900

    first commit

---
+++ backend/web-back/config/local_settings.py
@@ -0,0 +1,17 @@
+
+from .settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todoList',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
